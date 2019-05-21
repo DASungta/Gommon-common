616c6a0e69dfdcee9d08f4f8ab9341cf5b9debac
zhangyc <z.y.c.7777@163.com>
2019-05-21 08:46:14 +0800

    更新返回实体

---
+++ src/common/response/ResultResponse.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace common\response;
+
+use common\vo\Result;
+use think\Response;
+
+/**
+ * 适用ThinkPHP-5.1.x
+ * Created by PhpStorm.
+ * User: dasungta
+ * Date: 2019-03-25
+ * Time: 10:20
+ */
+class ResultResponse extends Response
+{
+    public static function success($data, $message = 'OK', $header = [], $options = [])
+    {
+        $result = new Result();
+        $data = $result->success($data, $message);
+        self::create($data, 'json', 200, $header, $options);
+    }
+
+    public static function error($key, $message, $data, $header = [], $options = [])
+    {
+        $result = new Result();
+        $data = $result->error($key, $message, $data);
+        self::create($data, 'json', 200, $header, $options);
+    }
+
+
+}
